59587801ce7013ff2498a18ce0914d32dabf3ecc
daglus <daglus.ck.ua@gmail.com>
2019-09-17 14:34:00 +0300

    Update user model, add lengt validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  has_many :tasks
-  
+  has_many :tasks, dependent: :destroy
+
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :validatable, :confirmable
 
   validates_presence_of :first_name, :last_name
+  validates :first_name, :last_name, length: { minimum: 3 }
 end
